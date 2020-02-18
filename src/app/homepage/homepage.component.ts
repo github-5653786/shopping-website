d863811375f4ba994f019e9704f9d5726912caad
Mahammed Mustafa Kaladagi <Mahammed.Kaladagi@godigit.com>
2020-02-18 11:20:13 +0530

    first commit

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -10,6 +10,23 @@ export class HomepageComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const images = document.querySelectorAll("[data-src]");
+    const imgob = new IntersectionObserver((entries, imgobserver) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          var src = entry.target.getAttribute("data-src");
+          entry.target.setAttribute("src", src);
+          entry.target.classList.add("loaded");
+          imgobserver.unobserve(entry.target);
+        }
+      })
+    });
+
+    images.forEach(image => {
+      imgob.observe(image);
+    });
   }
 
 }
